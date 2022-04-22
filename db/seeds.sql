4eca254ad10b24a27f9e41e6ee3725514be9421a
Your Name <felix.acevedo.tech@gmail.com>
2022-04-22 13:18:46 -0400

    working on db connection

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,14 @@
-INSERT INTO department (id, name)
+INSERT INTO department 
+    (id, name)
 VALUES 
-  (?,?);
+    (?,?);
 
-INSERT INTO role (id, title, salary, department_id) 
+INSERT INTO role 
+    (id, title, salary, department_id) 
 VALUES
-  (?,?,?,?);
+    (?,?,?,?);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
+INSERT INTO employee 
+    (id, first_name, last_name, role_id, manager_id) 
 VALUES 
     (?,?,?,?,?);
--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password}
+      password: '',
+      //port
+      port: 3006,
+      //database name
+      database: 'business_db'
+    },
+    console.log(`Connected to the business_db database.`)
+  );
+
+
+
